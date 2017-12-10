f0e05354ee6fa96babbaa78ba65a3d300ee57b78
Ruslan <ruslanbk11@mail.ru>
2017-12-10 11:34:29 +0300

    Changed order of posts

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
