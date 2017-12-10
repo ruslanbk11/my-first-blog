a48af8fb88fc9861977b5e7f3fecbbd4b1f6715b
Ruslan <ruslanbk11@mail.ru>
2017-12-10 23:51:17 +0300

    Added deleting, it doesn't work

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 ]
