c01296979d020ac6c137139b3a81503998e37229
Ruslan <ruslanbk11@mail.ru>
2017-12-14 02:02:35 +0300

    Added registration and authentication of new users

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^auth/login/$', views.login, name='login'),
+    url(r'^auth/logout/$', views.logout, name='logout'),
+    url(r'^auth/register/$', views.register, name='register'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'username': auth.get_user(request).username})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post, 'username': auth.get_user(request).username})
 
 def post_new(request):
     form = PostForm()
@@ -46,3 +48,36 @@ def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def login(request):
+    args = {}
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            args['login_error'] = "Пользователь не найден"
+            return render(request, 'blog/login.html', args)
+    else:
+        return render(request, 'blog/login.html', args)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def register(request):
+    args = {}
+    args['form'] = UserCreationForm(request.POST)
+    if request.POST:
+        new_user_form = UserCreationForm(request.POST)
+        if new_user_form.is_valid():
+            new_user_form.save()
+            new_user = auth.authenticate(username = new_user_form.cleaned_data['username'], password = new_user_form.cleaned_data['password1'])
+            auth.login(request, new_user)
+            return redirect('/')
+        else:
+            args['form'] = new_user_form
+    return render(request, 'blog/register.html', args)
